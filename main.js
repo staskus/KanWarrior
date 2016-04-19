2ffa8e34352afa0d60104f0babdf1c6b42a10038
Saonela <777skumbre@gmail.com>
2016-04-19 18:13:39 +0200

    Pradzia

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ var http = require("http");
 var fs = require('fs');
 var url = require('url');
 var task = require("./lib/Task");
+var express = require('express');
 
-task.getOutput("list",(text)=> {
-});
+var app = express();
+
+//task.getOutput("list",(text)=> {
+//});
 
 
 exec(cmd, function(error, stdout, stderr) {
@@ -15,34 +18,19 @@ exec(cmd, function(error, stdout, stderr) {
   text=stdout;
 });
 
-// Create a server
-http.createServer( function (request, response) {  
-   // Parse the request containing file name
-   var pathname = url.parse(request.url).pathname;
-   
-   // Print the name of the file for which request is made.
-   console.log("Request for " + pathname + " received.");
-   
-   // Read the requested file content from file system
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP Status: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else{	
-         //Page found	  
-         // HTTP Status: 200 : OK
-         // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});	
-         
-         // Write the content of the file to response body
-         response.write(data.toString());		
-      }
-      // Send the response body 
-      response.end();
-   });   
-}).listen(8081);
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname + '/public'));
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+app.get('/', function (req, res) {
+   //res.send('Hello World');
+   res.render('index.html');
+})
+
+var server = app.listen(8081, function () {
+
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+
+})
